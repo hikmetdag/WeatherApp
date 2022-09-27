3bec96ba36b49f88967cfa8b30f1b74e4ebc9aba
Hikmet Dag <temkihgad@gmail.com>
2022-09-27 16:57:52 +0200

    change

--- views/forecastView.js
+++ views/forecastView.js
@@ -1,14 +1,14 @@
-import { getDayName} from '../../utils/getDayNamePage.js'
+import { getDayName } from '../utils/getDayNamePage.js'
 
 export const forecastWeather = (data) => {
     const forecastCard = document.querySelector('.forecastCard')
     forecastCard.innerHTML = ''
     data.forEach(item => {
-        let x = String.raw`
+        let futureWeather = String.raw`
     <div class='forecastRow'>
         <h1 class='futureDay'>${getDayName(item)}</h1>
         <div class='imgBox'>
-        <img src='http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png' alt="weather img">
+            <img src='http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png' alt="weather img">
         </div>
         <div class="tempForecast">
             <span class="num">${Math.floor(item.main.temp)}</span>
@@ -17,7 +17,7 @@ export const forecastWeather = (data) => {
     </div>
    
         `
-        forecastCard.insertAdjacentHTML('beforeend', x);
+        forecastCard.insertAdjacentHTML('beforeend', futureWeather);
     })
 
 
